5a382a6da1e0c61809005e662283144586372c39
Collins Liyosi <collinskivale@gmail.com>
2018-08-19 19:59:55 +0300

    Add owners index template and controller. Closes #7

---
+++ pet-clinic-web/src/main/java/com/liyosi/springpetclinic/controllers/OwnerController.java
@@ -0,0 +1,17 @@
+package com.liyosi.springpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by liyosi on Aug, 2018
+ */
+@Controller
+@RequestMapping("/owners")
+public class OwnerController {
+
+  @RequestMapping({"", "/", "/index", "/index.html"})
+  public String listOwners() {
+    return "owners/index";
+  }
+}
