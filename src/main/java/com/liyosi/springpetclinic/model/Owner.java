a03d3d087ad439b7cd489bcbb8d0b1040afacee1
Collins Liyosi <collinskivale@gmail.com>
2018-08-19 14:55:38 +0300

    Add Pojos
    - Closes #2

---
+++ src/main/java/com/liyosi/springpetclinic/model/Owner.java
@@ -0,0 +1,7 @@
+package com.liyosi.springpetclinic.model;
+
+/**
+ * Created by liyosi on Aug, 2018
+ */
+public class Owner extends Person {
+}
---
+++ src/main/java/com/liyosi/springpetclinic/model/PetType.java
@@ -0,0 +1,17 @@
+package com.liyosi.springpetclinic.model;
+
+/**
+ * Created by liyosi on Aug, 2018
+ */
+public class PetType {
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  private String name;
+}
