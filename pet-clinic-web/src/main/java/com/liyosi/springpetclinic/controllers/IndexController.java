d370850e3a23e415d8ebbaf0d8433f6a77ab45d9
Collins Liyosi <collinskivale@gmail.com>
2018-08-19 19:30:14 +0300

    Add Index template and controller
    - Closes #13

---
+++ pet-clinic-web/src/main/java/com/liyosi/springpetclinic/controllers/IndexController.java
@@ -0,0 +1,16 @@
+package com.liyosi.springpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by liyosi on Aug, 2018
+ */
+@Controller
+public class IndexController {
+
+  @RequestMapping({"/", "", "index", "index.html"})
+  public String index() {
+    return "index";
+  }
+}
