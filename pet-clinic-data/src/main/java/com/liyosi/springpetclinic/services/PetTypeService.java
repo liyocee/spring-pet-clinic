ef072512e26146bfce842541bd9e9f8443c0b0df
Collins Liyosi <collinskivale@gmail.com>
2018-08-27 15:17:35 +0300

    Implement PetTypeMap services. closes #29

---
+++ pet-clinic-data/src/main/java/com/liyosi/springpetclinic/services/PetTypeService.java
@@ -0,0 +1,8 @@
+package com.liyosi.springpetclinic.services;
+
+import com.liyosi.springpetclinic.model.PetType;
+
+/**
+ * Created by liyosi on Aug, 2018
+ */
+public interface PetTypeService extends CrudService<PetType, Long> { }
---
+++ pet-clinic-data/src/main/java/com/liyosi/springpetclinic/services/map/PetTypeMapService.java
@@ -0,0 +1,10 @@
+package com.liyosi.springpetclinic.services.map;
+
+import com.liyosi.springpetclinic.model.PetType;
+import com.liyosi.springpetclinic.services.PetTypeService;
+
+/**
+ * Created by liyosi on Aug, 2018
+ */
+public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService {
+}
