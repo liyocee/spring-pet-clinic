37fcc0f6d4ebdad1f0ad2dfe64b89b5fc81ac697
Collins Liyosi <collinskivale@gmail.com>
2018-08-27 15:04:28 +0300

    Add visits, updated owner with Pet. closes #26

--- pet-clinic-data/src/main/java/com/liyosi/springpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/liyosi/springpetclinic/model/Owner.java
@@ -1,7 +1,22 @@
 package com.liyosi.springpetclinic.model;
 
+import java.util.Set;
+
 /**
  * Created by liyosi on Aug, 2018
  */
 public class Owner extends Person {
+  private Set<Pet> pets;
+
+  public Owner(Set<Pet> pets) {
+    this.pets = pets;
+  }
+
+  public Set<Pet> getPets() {
+    return pets;
+  }
+
+  public void setPets(Set<Pet> pets) {
+    this.pets = pets;
+  }
 }
