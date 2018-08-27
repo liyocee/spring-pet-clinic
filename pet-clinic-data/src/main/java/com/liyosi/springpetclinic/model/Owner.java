9d45ea0e5f48e8374cb7857a23f44641f1cda45a
Collins Liyosi <collinskivale@gmail.com>
2018-08-27 15:08:55 +0300

    Enhance owner with contact info. closes #28

--- pet-clinic-data/src/main/java/com/liyosi/springpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/liyosi/springpetclinic/model/Owner.java
@@ -7,9 +7,15 @@ import java.util.Set;
  */
 public class Owner extends Person {
   private Set<Pet> pets;
+  private String address;
+  private String city;
+  private String telephone;
 
-  public Owner(Set<Pet> pets) {
+  public Owner(Set<Pet> pets, String address, String city, String telephone) {
     this.pets = pets;
+    this.address = address;
+    this.city = city;
+    this.telephone = telephone;
   }
 
   public Set<Pet> getPets() {
@@ -18,5 +24,29 @@ public class Owner extends Person {
 
   public void setPets(Set<Pet> pets) {
     this.pets = pets;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public void setAddress(String address) {
+    this.address = address;
+  }
+
+  public String getCity() {
+    return city;
+  }
+
+  public void setCity(String city) {
+    this.city = city;
+  }
+
+  public String getTelephone() {
+    return telephone;
+  }
+
+  public void setTelephone(String telephone) {
+    this.telephone = telephone;
   }
 }
