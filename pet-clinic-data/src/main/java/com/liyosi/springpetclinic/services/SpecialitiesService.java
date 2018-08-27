c39a85520964930c9285ab96e353aa0ed59340d3
Collins Liyosi <collinskivale@gmail.com>
2018-08-27 16:26:23 +0300

    Implement Speciality servie. Closes #31

---
+++ pet-clinic-data/src/main/java/com/liyosi/springpetclinic/services/SpecialitiesService.java
@@ -0,0 +1,9 @@
+package com.liyosi.springpetclinic.services;
+
+import com.liyosi.springpetclinic.model.Speciality;
+
+/**
+ * Created by liyosi on Aug, 2018
+ */
+public interface SpecialitiesService extends CrudService<Speciality, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/liyosi/springpetclinic/services/map/SpecialitiesMapService.java
@@ -0,0 +1,10 @@
+package com.liyosi.springpetclinic.services.map;
+
+import com.liyosi.springpetclinic.model.Speciality;
+import com.liyosi.springpetclinic.services.SpecialitiesService;
+
+/**
+ * Created by liyosi on Aug, 2018
+ */
+public class SpecialitiesMapService extends AbstractMapService<Speciality, Long> implements SpecialitiesService {
+}
