08dd475f3f1845acde089b5d733c81af36e211cd
Collins Liyosi <collinskivale@gmail.com>
2018-08-27 16:58:58 +0300

    Fix broken links

--- pet-clinic-web/src/main/java/com/liyosi/springpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/liyosi/springpetclinic/controllers/OwnerController.java
@@ -25,5 +25,10 @@ public class OwnerController {
 
     return "owners/index";
   }
+
+  @RequestMapping({"/find", "oups"})
+  public String findOwners() {
+    return "notimplemented.html";
+  }
 }
 
