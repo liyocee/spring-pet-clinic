c5142d44fb5a6157eb96a1309999fa518bd2c7fb
Collins Liyosi <collinskivale@gmail.com>
2018-08-29 18:47:25 +0300

    Implement base entity

--- pet-clinic-data/src/main/java/com/liyosi/springpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/liyosi/springpetclinic/model/BaseEntity.java
@@ -1,11 +1,19 @@
 package com.liyosi.springpetclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
 /**
  * Created by liyosi on Aug, 2018
  */
+@MappedSuperclass
 public class BaseEntity implements Serializable {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
   public Long getId() {
