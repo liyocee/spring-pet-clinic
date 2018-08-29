fb81cdc3d42a848a7295921a2881c5a81d3b68f5
Collins Liyosi <collinskivale@gmail.com>
2018-08-29 19:46:49 +0300

    Impelment Vet Service

---
+++ pet-clinic-data/src/main/java/com/liyosi/springpetclinic/services/jpa/VetService.java
@@ -0,0 +1,51 @@
+package com.liyosi.springpetclinic.services.jpa;
+
+import com.liyosi.springpetclinic.model.Vet;
+import com.liyosi.springpetclinic.repositories.VetRepository;
+import com.liyosi.springpetclinic.services.CrudService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by liyosi on Aug, 2018
+ */
+@Service
+@Profile("springdatajpa")
+public class VetService implements CrudService<Vet, Long> {
+
+  private final VetRepository vetRepository;
+
+  public VetService(VetRepository vetRepository) {
+    this.vetRepository = vetRepository;
+  }
+
+  @Override
+  public Vet findById(Long aLong) {
+    return vetRepository.findById(aLong).orElse(null);
+  }
+
+  @Override
+  public Vet save(Vet object) {
+    return vetRepository.save(object);
+  }
+
+  @Override
+  public Set<Vet> findAll() {
+    Set<Vet> vets = new HashSet<>();
+    vetRepository.findAll().forEach(vets::add);
+    return vets;
+  }
+
+  @Override
+  public void delete(Vet object) {
+    vetRepository.delete(object);
+  }
+
+  @Override
+  public void deleteById(Long aLong) {
+    vetRepository.deleteById(aLong);
+  }
+}
