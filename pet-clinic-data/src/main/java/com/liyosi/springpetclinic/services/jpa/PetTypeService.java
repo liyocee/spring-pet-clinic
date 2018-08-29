3a0c02dc3324672d166e603c20aa52772540eccb
Collins Liyosi <collinskivale@gmail.com>
2018-08-29 19:51:11 +0300

    Implement PetType service

---
+++ pet-clinic-data/src/main/java/com/liyosi/springpetclinic/services/jpa/PetTypeService.java
@@ -0,0 +1,50 @@
+package com.liyosi.springpetclinic.services.jpa;
+
+import com.liyosi.springpetclinic.model.PetType;
+import com.liyosi.springpetclinic.repositories.PetTypeRepository;
+import com.liyosi.springpetclinic.services.CrudService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by liyosi on Aug, 2018
+ */
+@Service
+@Profile("springdatajpa")
+public class PetTypeService implements CrudService<PetType, Long> {
+  private final PetTypeRepository petTypeRepository;
+
+  public PetTypeService(PetTypeRepository petTypeRepository) {
+    this.petTypeRepository = petTypeRepository;
+  }
+
+  @Override
+  public PetType findById(Long aLong) {
+    return petTypeRepository.findById(aLong).orElse(null);
+  }
+
+  @Override
+  public PetType save(PetType object) {
+    return petTypeRepository.save(object);
+  }
+
+  @Override
+  public Set<PetType> findAll() {
+    Set<PetType> petTypes = new HashSet<>();
+    petTypeRepository.findAll().forEach(petTypes::add);
+    return petTypes;
+  }
+
+  @Override
+  public void delete(PetType object) {
+    petTypeRepository.delete(object);
+  }
+
+  @Override
+  public void deleteById(Long aLong) {
+    petTypeRepository.deleteById(aLong);
+  }
+}
