2c9d637d179640cb7fa8330a90f4b3aa4d1b0052
Collins Liyosi <collinskivale@gmail.com>
2018-08-29 19:09:23 +0300

    Create vet entity

--- pet-clinic-data/src/main/java/com/liyosi/springpetclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/com/liyosi/springpetclinic/model/Speciality.java
@@ -1,10 +1,17 @@
 package com.liyosi.springpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 /**
  * Created by liyosi on Aug, 2018
  */
+@Entity
+@Table(name = "specialities")
 public class Speciality extends BaseEntity {
 
+  @Column(name = "description")
   private String description;
 
   public Speciality() {
--- pet-clinic-data/src/main/java/com/liyosi/springpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/liyosi/springpetclinic/model/Vet.java
@@ -1,13 +1,18 @@
 package com.liyosi.springpetclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
 /**
  * Created by liyosi on Aug, 2018
  */
+@Entity
+@Table(name = "vet")
 public class Vet extends Person {
 
+  @ManyToMany(fetch = FetchType.EAGER)
+  @JoinTable(name = "vet_specialities", joinColumns = @JoinColumn(name = "vet_id"), inverseJoinColumns = @JoinColumn(name = "speciality_id"))
   private Set<Speciality> specialities = new HashSet<>();
 
   public Vet() {
