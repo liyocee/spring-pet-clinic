a36b090019791ebd320ae3ef41774d02c7da92a9
Collins Liyosi <collinskivale@gmail.com>
2018-08-29 19:43:07 +0300

    Add Owners Service

---
+++ pet-clinic-data/src/main/java/com/liyosi/springpetclinic/services/jpa/OwnerService.java
@@ -0,0 +1,56 @@
+package com.liyosi.springpetclinic.services.jpa;
+
+import com.liyosi.springpetclinic.model.Owner;
+import com.liyosi.springpetclinic.repositories.OwnerRepository;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by liyosi on Aug, 2018
+ */
+@Service
+@Profile("springdatajpa")
+public class OwnerService implements com.liyosi.springpetclinic.services.OwnerService {
+
+  private final OwnerRepository ownerRepository;
+
+  public OwnerService(OwnerRepository ownerRepository) {
+    this.ownerRepository = ownerRepository;
+  }
+
+  @Override
+  public Owner findByLastName(String lastName) {
+    return ownerRepository.findByLastName(lastName);
+  }
+
+  @Override
+  public Owner findById(Long aLong) {
+    return ownerRepository.findById(aLong).orElse(null);
+  }
+
+  @Override
+  public Owner save(Owner object) {
+    return ownerRepository.save(object);
+  }
+
+  @Override
+  public Set<Owner> findAll() {
+    Set<Owner> owners = new HashSet<>();
+    ownerRepository.findAll().forEach(owners::add);
+    return owners ;
+  }
+
+  @Override
+  public void delete(Owner object) {
+    ownerRepository.delete(object);
+  }
+
+  @Override
+  public void deleteById(Long aLong) {
+    ownerRepository.deleteById(aLong);
+
+  }
+}
